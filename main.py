00d6174e21e5056a75c8c34c7675add6ca674a09
Artem Son <s0sis0n@yandex.ru>
2024-01-22 11:27:49 +0300

    update output probability

--- main.py
+++ main.py
@@ -5,16 +5,34 @@ from model import model_build, ModelType
 
 
 if __name__ == "__main__":
+    generes = [
+        "blues",
+        "classical",
+        "country",
+        "disco",
+        "hiphop",
+        "jazz",
+        "metal",
+        "pop",
+        "reggae",
+        "rock",
+    ]
+
     split = KFold(n_splits=3, shuffle=True)
 
-    model = model_build(ModelType.KNN, split)
+    model = model_build(ModelType.RFC, split)
 
-    file_path = "./Data/genres_original/rock/rock.00089.wav"
+    file_path = "./test/Казённый унитаз - Моча съела говно.mp3"
     with open(file_path, "r") as file:
         features = feature_extractor(file)
 
     pred = model.predict(features)
     prob = model.predict_proba(features)
+    print(prob)
+
+    res = {}
+    for index, key in enumerate(generes):
+        res[key] = prob[0][index]
 
     print(f"Model prediction: {pred}")
-    print(f"Probability: {prob}")
+    print(f"Probability: {res}")
